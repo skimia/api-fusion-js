1bb06dab940a56299a04be6f72a9c8c451f94d52
Kessler <kessler.dev@gmail.com>
2016-02-11 12:40:26 +0100

    First Commit

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var $gulp = require('gulp');
+
+var $ts =       require('gulp-typescript');
+var $sass =      require('gulp-sass');
+var $template =  require('gulp-templatecache');
+var $live =      require('gulp-livereload');
+
+var sassConfig = {
+    onError: console.error.bind(console, 'SASS Error:')
+}
+
+$gulp.task('ts', function() {
+    return $gulp.src('src/ts/**/*.ts')
+        .pipe($ts({
+            out: 'app.js',
+            target: 'ES5'
+        }))
+        .pipe($gulp.dest('dist/js'));
+});
+$gulp.task('tst', function() {
+    return $gulp.src('src/ts/**/*.html')
+        .pipe($template({
+            root: '/ts',
+            moduleName: 'jstack',
+            output: 'app-templates.js',
+            strip: __dirname +'/ts/'
+        }))
+        .pipe($gulp.dest('dist/js'));
+});
+
+$gulp.task('sass', function(){
+
+    return $gulp.src('src/sass/**/*.scss')
+        .pipe($sass(sassConfig))
+        .pipe($gulp.dest('dist/css'));
+});
+
+$gulp.task('watch', ['ts','tst'], function() {
+
+    $live.listen();
+
+    $gulp.watch('src/ts/**/*.ts', ['ts'])
+        .on('change', function(event){
+            console.info('-----Change detected on "' + event.path +'"-----');
+            //$live.changed(event.path);
+        });
+    $gulp.watch('src/ts/**/*.html', ['tst'])
+        .on('change', function(event){
+            console.info('-----Change detected on "' + event.path +'"-----');
+            $live.changed(event.path);
+        });
+
+    $gulp.watch('src/sass/**/*.scss', ['sass'])
+        .on('change', function(event){
+            console.info('-----Change detected on "' + event.path +'"-----');
+            $live.changed(event.path);
+        });
+
+    $gulp.watch(['dist/js/**/*.js','dist/css/**/*.css'])
+        .on('change', function(event){
+            $live.changed(event.path);
+        });
+});
---
+++ src/ts/core/FusionCore.ts
@@ -0,0 +1,7 @@
+
+/// <reference path="../vendors/framework.ts"/>
+module Skimia.Fusion.Core{
+
+    export var module = new Skimia.Angular.Module('fusion.core', []);
+}
+
